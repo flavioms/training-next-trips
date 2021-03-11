b5bcf2e8f4f557dbf756649d63463a34728f7f0f
flavioms <flaviomartins_silva@hotmail.com>
2021-03-11 17:35:08 -0300

    feat: add generated pages places

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,31 @@
+import * as S from './styles'
+
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlaceTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlaceTemplateProps) {
+  return (
+    <S.Wrapper>
+      <h1>{place.name}</h1>
+      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+
+      {place.gallery.map((image, index) => (
+        <img src={image.url} alt={place.name} key={`photo-${index}`} />
+      ))}
+    </S.Wrapper>
+  )
+}
