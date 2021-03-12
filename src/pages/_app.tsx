14b3e0929196e378eb9079fa76bda9a4ea878d7a
flavioms <flaviomartins_silva@hotmail.com>
2021-03-12 09:11:02 -0300

    feat: add nprogress to pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import NextNprogress from 'nextjs-progressbar'
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
@@ -23,6 +24,12 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <GlobalStyles />
+      <NextNprogress
+        color="#f231a5"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={6}
+      />
       <Component {...pageProps} />
     </>
   )
